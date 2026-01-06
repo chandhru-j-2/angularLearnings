356774c28690bc6ece4540da8fe1fc45edb94f9a
Chandhru Jayakumar <Chandhru.Jayakumar@valuemomentum.com>
2026-01-06 15:16:51 +0530

    Day 2: OnInit - LifeCycle Method

--- src/app/todo-list/todo-list.ts
+++ src/app/todo-list/todo-list.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Todos } from '../models/todos';
-import { Title } from '@angular/platform-browser';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
@@ -10,28 +10,34 @@ import { Title } from '@angular/platform-browser';
 })
 export class TodoList {
   todoList: Todos[] = [];
-
+  ngOnInit(): void {
+    let savedTodos = localStorage.getItem('todos');
+    this.todoList = savedTodos ? JSON.parse(savedTodos) : [];
+  }
   newTitle: string = '';
   newDesc: string = '';
   newDate: Date = new Date();
 
   addTodo() {
-    if(this.newTitle.trim().length && this.newDesc && this.newDate){
+    if (this.newTitle.trim().length && this.newDesc && this.newDate) {
       let newTodo: Todos = {
         id: Date.now(),
         title: this.newTitle,
         desc: this.newDesc,
-        date: this.newDate
-      }
+        date: this.newDate,
+      };
       this.todoList.push(newTodo);
 
       this.newTitle = '';
       this.newDesc = '';
       this.newDate = new Date();
+
+      localStorage.setItem('todos', JSON.stringify(this.todoList));
     }
   }
 
-  deleteTodo(index: number){
-    this.todoList.splice(index,1);
+  deleteTodo(index: number) {
+    this.todoList.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(this.todoList));
   }
 }
